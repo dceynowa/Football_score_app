219e990ff054fe32839d6f439eaebc250973c2ad
Dawid Ceynowa <dejw.ce@gmail.com>
2019-01-16 18:22:16 +0100

    Show teams on list group

--- src/components/Teams/index.js
+++ src/components/Teams/index.js
@@ -1,24 +1,48 @@
 import React, { Component } from 'react'
+import { Grid, Row, Col, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 import {BASIC_URL} from '../../url/url'
 import {HEADER} from '../../auth/index' 
 
 class Teams extends Component {
     
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            teams: [],
+        };
+    }
+
+
     
 
     componentDidMount() {
         fetch(`${BASIC_URL}/v2/competitions/PL/teams`, HEADER)
         .then(res => res.json())
-        .then(res => console.log(res))
+        .then(res => this.setState({teams: res.teams}))
         .catch(err => console.error(err));
     }
+    
 
     render () {
+
+        const { teams } = this.state;
+
         return (
-            <div>
-                COMPETITIONS    
-            </div>
+            <Grid>
+                <Row className="show-grid">
+                    <Col md={8} mdOffset={2}>
+                        <ListGroup id="list_of_teams" >Teams in Premier League:
+                            {teams.map(team =>
+                                <ListGroupItem key={team.id}>
+                                    {team.name}
+                                </ListGroupItem>
+                            )}
+                        </ListGroup>
+                    </Col>
+                </Row>
+            </Grid>
         )
     }
 }
