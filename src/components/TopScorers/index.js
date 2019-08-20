b7e44c242bd707256f89a26a941f5adec3f0b14a
Dawid Ceynowa <dejw.ce@gmail.com>
2019-08-20 14:53:32 +0200

    add topScorers and add some national flags

--- src/components/TopScorers/index.js
+++ src/components/TopScorers/index.js
@@ -2,10 +2,16 @@ import React, { Component } from "react"
 
 import { Table } from "react-bootstrap"
 
-import { BASIC_URL } from "../../url/url"
+import Flag from "../Flag"
+
+import { BASIC_URL, COUNTRY_API_URL } from "../../url/url"
 import { HEADER } from "../../auth/index"
 
 class TopScorers extends Component {
+  state = {
+    scorers: []
+  }
+
   componentDidMount() {
     const { league_code } = this.props
     fetch(
@@ -13,13 +19,41 @@ class TopScorers extends Component {
       HEADER
     )
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => this.setState({ scorers: data.scorers }))
   }
-
   render() {
+    let scorers = [...this.state.scorers]
+    const tableBody = scorers.map((item, index) => (
+      <tr key={item.player.id}>
+        <td className="text-center">{index + 1}</td>
+        <td>
+          <div className="nationalFlag">
+            {item.player.name}
+            <Flag
+              nationality={item.player.nationality}
+              className="topScorers_countryFlag_img"
+            />
+          </div>
+        </td>
+
+        <td>{item.team.name}</td>
+        <td className="text-center">{item.numberOfGoals}</td>
+      </tr>
+    ))
+
     return (
       <>
-        <h4>Strzelcy</h4>
+        <Table striped hover responsive className="text-left">
+          <thead>
+            <tr>
+              <th className="text-center">#</th>
+              <th>ZAWODNIK</th>
+              <th>KLUB</th>
+              <th className="text-center">GOLE</th>
+            </tr>
+          </thead>
+          <tbody>{tableBody}</tbody>
+        </Table>
       </>
     )
   }
