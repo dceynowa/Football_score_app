7b0f2102e3dc78fd51ffe1c95b8ab715549efb33
Dawid Ceynowa <dejw.ce@gmail.com>
2019-08-20 10:19:35 +0200

    styled league table

--- src/components/TabbedLeagueTable/index.js
+++ src/components/TabbedLeagueTable/index.js
@@ -8,16 +8,20 @@ import "./style.css"
 const TabbedLeagueTable = props => {
   const { league_code } = props.match.params
   return (
-    <Tabs defaultActiveKey={1} id="tabbed-league-table" hovered>
-      <Tab className="tabs_league_table" eventKey={1} title="Ogółem">
+    <Tabs
+      defaultActiveKey={1}
+      id="tabbed-league-table"
+      className="tabs_league_table"
+    >
+      <Tab className="tab_league_table" eventKey={1} title="Ogółem">
         {<LeagueTable league_code={league_code} type="TOTAL" />}
       </Tab>
 
-      <Tab className="tabs_league_table" eventKey={2} title="U siebie">
+      <Tab className="tab_league_table" eventKey={2} title="U siebie">
         <LeagueTable league_code={league_code} type="HOME" />
       </Tab>
 
-      <Tab className="tabs_league_table" eventKey={3} title="Na wyjeździe">
+      <Tab className="tab_league_table" eventKey={3} title="Na wyjeździe">
         <LeagueTable league_code={league_code} type="AWAY" />
       </Tab>
     </Tabs>
